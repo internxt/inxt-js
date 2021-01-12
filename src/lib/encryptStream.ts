7deb89c1e547df27a23d0721c9ff25774a507a29
Joan Mora <elcorreudejoanmora@gmail.com>
2021-01-12 23:52:42 +0100

    Test Encryption and decryption Pipelines

--- src/lib/encryptStream.ts
+++ src/lib/encryptStream.ts
@@ -3,14 +3,17 @@ import { createCipheriv, Cipher} from 'crypto'
 
 export class EncryptStream extends Transform {
   private cipher: Cipher
+  public encrypted_bytes: number
 
   constructor(key: Buffer, iv: Buffer) {
     super()
+    this.encrypted_bytes = 0
     this.cipher = createCipheriv('aes-256-ctr', key, iv)
   }
 
   _transform(chunk: Buffer, enc: string, cb: (err: Error | null, data: Buffer) => void): void {
     this.cipher.write(chunk);
+    this.encrypted_bytes += Buffer.byteLength(chunk)
     cb(null, this.cipher.read());
   }
 
---
+++ tests/encryption-decryption-streams.ts
@@ -0,0 +1,64 @@
+import DecryptStream from '../src/lib/decryptstream'
+import EncryptStream from '../src/lib/encryptStream'
+import { createCipheriv, randomBytes, createDecipheriv, createHash } from 'crypto'
+import stream from 'stream'
+import expect from 'chai'
+import assert from 'assert'
+
+// Crypto encryption mock
+
+describe('# Encryption - Decryption logic', () => {
+  const algorithm = 'aes-256-ctr'
+  const secret = '12345678123456781234567812345678'
+  const keyRaw = createHash('sha256').update(String(secret)).digest('base64').substr(0, 32);
+  const key = Buffer.from(keyRaw, 'utf-8')
+  const iv = randomBytes(16)
+
+  const cipher = createCipheriv(algorithm, key, iv);
+
+  const text = 'Joan Mora'
+
+  let encryptedCrypto = cipher.update(text)
+  encryptedCrypto = Buffer.concat([encryptedCrypto, cipher.final()])
+
+  const decipher = createDecipheriv(algorithm, key, iv)
+  let decryptedCrypto = decipher.update(encryptedCrypto)
+  decryptedCrypto = Buffer.concat([decryptedCrypto, decipher.final()])
+
+  it('Check encryption produces same output as encrypting with crypto', () => {
+    const encryptedStream = new EncryptStream(key, iv)
+    let data: string = ''
+    encryptedStream.on('data', (chunk) => data += chunk.toString())
+    encryptedStream.on('end', () => assert.strictEqual(data, encryptedCrypto.toString()))
+    encryptedStream.write(Buffer.from(text))
+    encryptedStream.end()
+
+  })
+
+  it('Check decryption produces the same output as crypto', () => {
+    const decryptStream = new DecryptStream(key, iv)
+    let data : string = ''
+    decryptStream.on('data', (chunk) => data += chunk.toString())
+    decryptStream.on('end', () => assert.strictEqual(data, decryptedCrypto.toString()))
+    decryptStream.write(encryptedCrypto)
+    decryptStream.end()
+  })
+
+  it('Check LOCAL-UPLOAD encryption - decryption pipeline', () => {
+    const contentStream = new stream.PassThrough()
+    let content = ''
+    contentStream.on('data', (chunk) => content += chunk.toString())
+    contentStream.on('end', () => assert.strictEqual(content, text))
+
+    const readableStreeamInput = new stream.PassThrough()
+    readableStreeamInput.end(Buffer.from(text))
+
+    readableStreeamInput
+      .pipe(new EncryptStream(key, iv))
+      .pipe(new DecryptStream(key, iv))
+      .pipe(contentStream)
+  })
+
+
+
+})
