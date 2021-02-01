a0661d58cfddc6eca44bb98cf86763706a102cb6
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-02-01 17:37:22 +0100

    update mirror tests + spawner

--- tests/mocks/mirror.unit.spec.ts
+++ tests/mocks/mirror.unit.spec.ts
@@ -1,12 +1,13 @@
-import { expect } from "chai"
+import { expect } from 'chai'
 import { randomBytes } from 'crypto'
 import { Readable } from 'stream'
 
-import { MirrorMock } from '../../src/lib/utils/mocks/bridge/mirror'
-import { NodeMock, NodeResponse } from '../../src/lib/utils/mocks/node'
+import { NodeResponse } from '../../src/lib/utils/mocks/node'
+import { spawn } from '../../src/lib/utils/mocks'
 
-const mirror = new MirrorMock()
-const node = new NodeMock(3000, '/path/to/node', randomBytes(32).toString('hex'), 'www.fakenode.com')
+const node   = spawn.node({})
+const bridge = spawn.bridge({})
+const mirror = spawn.mirror({ node, bridge })
 
 const shardStream = new Readable({
     read () {
@@ -16,15 +17,51 @@ const shardStream = new Readable({
 })
 
 describe('# Mirror', () => {
-    it('Should finish the upload correctly', async () => {
-        const successRes = new NodeResponse(true, 200, '', 32)
-        node.nodeResponse = successRes
-        expect(await mirror.UploadShard(node, shardStream)).to.be.true
-    })  
+    describe('UploadShard()', () => {
+        it('Should finish the upload correctly', async () => {
+            const successRes = new NodeResponse(true, 200, '', 32)
+            node.nodeResponse = successRes
+            expect(await mirror.UploadShard(node, shardStream)).to.be.true
+        })  
+    
+        it('Should finish the upload with error', async () => {
+            const errorRes = new NodeResponse(false, 401, '', 32)
+            node.nodeResponse = errorRes
+            expect(await mirror.UploadShard(node, shardStream)).to.be.false
+        })
+    })
 
-    it('Should finish the upload with error', async () => {
-        const errorRes = new NodeResponse(false, 401, '', 32)
-        node.nodeResponse = errorRes
-        expect(await mirror.UploadShard(node, shardStream)).to.be.false
+    describe('DownloadShardRequest()', () => {
+        it('Should return a readable with the injected size', async () => {
+            const size = 50
+            const successRes = new NodeResponse(true, 200, '', size)
+            node.nodeResponse = successRes
+
+            const config = { bridgeUser: 'fake', bridgePass: 'fake' }
+            const hash = randomBytes(32).toString('hex')
+            const token = randomBytes(32).toString('hex')
+
+            const shardStream = mirror.DownloadShardRequest(config, node.path, node.port, hash, token, node.ID)
+
+            expect(shardStream).to.be.instanceOf(Readable)
+            shardStream.on('end', () => expect(shardStream.readableLength).to.be.equal(size))
+        })  
+    })
+
+    describe('DownloadShard()', () => {
+        it('Should return a readable with the injected size', async () => {
+            const size = 50
+            const successRes = new NodeResponse(true, 200, '', size)
+            node.nodeResponse = successRes
+
+            const config = { bridgeUser: 'fake', bridgePass: 'fake' }
+            const hash = randomBytes(32).toString('hex')
+            const token = randomBytes(32).toString('hex')
+
+            const shardStream = mirror.DownloadShardRequest(config, node.path, node.port, hash, token, node.ID)
+
+            expect(shardStream).to.be.instanceOf(Readable)
+            shardStream.on('end', () => expect(shardStream.readableLength).to.be.equal(size))
+        })  
     })
 })
