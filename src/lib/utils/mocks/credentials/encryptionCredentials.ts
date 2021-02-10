91209d05c6b53e38ec80e72108666e233b162bd4
Joan Mora <elcorreudejoanmora@gmail.com>
2021-02-10 11:30:55 +0100

    Update mock

--- src/lib/utils/mocks/credentials/encryptionCredentials.ts
+++ src/lib/utils/mocks/credentials/encryptionCredentials.ts
@@ -2,11 +2,12 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const EncryptionCredentials = {
-  INDEX: Buffer.from('0000000000000000000000000000000000000000000000000000000000000000', "hex"),
+  INDEX: Buffer.from(process.env.TEST_INDEX, "hex"),
   MNEMONIC: process.env.TEST_KEY,
   BUCKET_ID: process.env.TEST_BUCKET_ID,
   BUCKET_KEY: process.env.TEST_BUCKET_KEY,
-  FILE_KEY: process.env.TEST_FILE_KEY
+  FILE_KEY: process.env.TEST_FILE_KEY,
+  IV: Buffer.from(process.env.TEST_IV, 'hex')
 }
 
 module.exports = EncryptionCredentials
