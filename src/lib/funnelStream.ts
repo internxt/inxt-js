90d663c2c6cf33ffba8e23de9fc57436deab968a
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-02-12 09:55:16 +0100

    Funnel stream to limit chunk sizes

---
+++ src/lib/funnelStream.ts
@@ -0,0 +1,35 @@
+import { Transform } from 'stream'
+
+export class FunnelStream extends Transform {
+    private limit: number;
+    private chunk: Buffer;
+
+    constructor (limit = 1) {
+        super()
+        this.limit = limit
+    }
+
+    _transform (chunk: Buffer) : void {
+        this.chunk = chunk
+        
+        let start = 0
+        let offset = 0
+        const end = chunk.byteLength
+        
+        while (start < end) {
+            offset = start + this.limit
+
+            if (offset > end) {
+                offset = end
+            }
+
+            this.push(chunk.slice(start, offset))
+
+            start += this.limit
+        }
+    }
+
+    _flush (cb: (err: Error | null, data: Buffer) => void) {
+        cb(null, this.chunk)
+    }
+}
