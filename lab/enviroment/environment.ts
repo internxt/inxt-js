350690f0d1fffa589fb1ad46de2880482aa7b3d0
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-03-08 13:02:18 +0100

    created lab environment for dev purposes

---
+++ lab/enviroment/environment.ts
@@ -0,0 +1,40 @@
+import { Readable } from 'stream'
+
+import { DownloadFileOptions, Environment, UploadFinishCallback, UploadProgressCallback } from "../../src"
+import { FileToUpload } from "../../src/api/shard"
+import { EncryptFilename } from "../../src/lib/crypto"
+import { Download } from "../../src/lib/download"
+import { Upload } from "../../src/lib/upload"
+
+export class LabEnvironment extends Environment {
+    /**
+     * Uploads a file
+     * @param bucketId Bucket id where file is
+     * @param file file metadata required for performing the upload
+     * @param progress Progress callback
+     * @param finish Finish callback
+     */
+    upload(bucketId: string, file: FileToUpload, progress: UploadProgressCallback, finish: UploadFinishCallback) : void {
+        if(!this.config.encryptionKey) {
+            throw new Error('Mnemonic was not provided')   
+        }
+
+        EncryptFilename(this.config.encryptionKey, bucketId, file.name).then((name: string) => {
+            Upload(this.config, bucketId, {...file, name}, progress, finish)
+        })
+    }
+
+    /**
+     * Downloads a file
+     * @param bucketId Bucket id where file is
+     * @param fileId Id of the file to be downloaded
+     * @param options Available options for download
+     */
+    download(bucketId: string, fileId: string, options: DownloadFileOptions) : Promise<Readable> {
+        if(!this.config.encryptionKey) {
+            throw new Error('Mnemonic was not provided')
+        }
+
+        return Download(this.config, bucketId, fileId, options)
+    }
+}
