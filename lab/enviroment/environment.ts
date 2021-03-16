72f74f74d610b6ca14d87bdeaab0a14691c9a76a
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-03-16 12:58:19 +0100

    centralize download logs

--- lab/enviroment/environment.ts
+++ lab/enviroment/environment.ts
@@ -5,6 +5,8 @@ import { FileMeta } from "../../src/api/FileObjectUpload"
 import { EncryptFilename } from "../../src/lib/crypto"
 import { Download } from "../../src/lib/download"
 import { Upload } from "../../src/lib/upload"
+import { logger } from '../../src/lib/utils/logger'
+import { ShardFailedIntegrityCheckError, ShardSuccesfulIntegrityCheck } from '../../src/lib/filemuxer'
 
 export class LabEnvironment extends Environment {
     /**
@@ -30,11 +32,44 @@ export class LabEnvironment extends Environment {
      * @param fileId Id of the file to be downloaded
      * @param options Available options for download
      */
-    download(bucketId: string, fileId: string, options: DownloadFileOptions) : Promise<Readable> {
+    async download(bucketId: string, fileId: string, options: DownloadFileOptions) : Promise<Readable> {
         if(!this.config.encryptionKey) {
             throw new Error('Mnemonic was not provided')
         }
 
-        return Download(this.config, bucketId, fileId, options)
+        const output = await Download(this.config, bucketId, fileId, options)
+
+        handleLogsFrom(output)
+
+        return output
     }
+}
+
+function handleLogsFrom(output: Readable) {
+    output.on('error', (err: Error) => {
+        logger.error('Download failed due to %s', err.message)
+        console.error(err)
+    })
+
+    handleDownloadLogs(output)
+    handleDecryptingLogs(output)
+
+    output.on('end', () => logger.info('Download complete.'))
+}
+
+function handleDownloadLogs(output: Readable) {
+    output.on('download-finished', () => logger.info('Shards download finished. Decrypting...'))
+
+    output.on('download-filemuxer-error', (err: ShardFailedIntegrityCheckError) => {
+        logger.error('%s. Expected hash: %s, actual: %s', err.message, err.content.expectedHash, err.content.actualHash)
+    })
+
+    output.on('download-filemuxer-success', (msg: ShardSuccesfulIntegrityCheck) => {
+        logger.debug('digest %s', msg.content.digest)
+        logger.info('shard %s OK', msg.content.expectedHash)
+    })
+}
+
+function handleDecryptingLogs(output: Readable) {
+    output.on('decrypting-finished', () => logger.info('Decrypting finished.'))
 }
