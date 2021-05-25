6504c80fed9d02e851f1d797dcf5c44c29e380b9
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-05-25 19:54:40 +0200

    StartUploadFile() basic tests

--- tests/api/FileObjectUpload.unit.ts
+++ tests/api/FileObjectUpload.unit.ts
@@ -1,10 +1,14 @@
 import { expect } from 'chai';
 import { Readable } from 'stream';
+import { resolve } from 'path';
 
 import { FileMeta, FileObjectUpload } from '../../src/api/FileObjectUpload';
 import { EnvironmentConfig } from '../../src';
 import { getShardMeta, ShardMeta } from '../../src/lib/shardMeta';
 import { sha512HmacBuffer } from '../../src/lib/crypto';
+
+import dotenv from 'dotenv';
+dotenv.config({ path: resolve(__dirname, '../../.env') });
 
 const fakeEnv: EnvironmentConfig = {
     bridgeUser: 'fake@user.com',
@@ -32,5 +36,58 @@ describe('# FileObjectUpload tests', () => {
             .digest('hex');
 
         expect(fileObject.GenerateHmac(shardMetas)).to.equal(expectedHmac);
+    });
+
+    describe('StartUploadFile()', () => {
+        it('Should throw an error if given bridgeUrl is wrong', async () => {
+            let errorThrown = false;
+
+            try {
+                const fileObject = await new FileObjectUpload(fakeEnv, fakeFileMeta, bucketId).init();
+                await fileObject.StartUploadFile();
+            } catch (err) {
+                errorThrown = true;
+                expect(err.message.includes('ECONNREFUSED')).to.be.true;
+            } finally  {
+                expect(errorThrown).to.be.true;
+            }      
+        });
+
+        it('Should throw an error of unauthorized request if given credentials are wrong', async () => {
+            let errorThrown = false;
+
+            try {
+                const overridenConfig = { ...fakeEnv, bridgeUrl: 'https://api.internxt.com' };
+                const fileObject = await new FileObjectUpload(overridenConfig, fakeFileMeta, bucketId).init();
+                await fileObject.StartUploadFile();
+            } catch (err) {
+                errorThrown = true;
+                expect(err.message).to.equal('Request failed with status code 401')
+            } finally  {
+                expect(errorThrown).to.be.true;
+            }      
+        });
+
+        it('Should throw an error if given bucketId is wrong', async function () {
+            // let errorThrown = false;
+            
+            // try {
+            //     const overridenConfig = { 
+            //         bridgeUser: process.env.TEST_USER,
+            //         bridgePass: process.env.TEST_PASS,
+            //         encryptionKey: process.env.TEST_KEY,
+            //         bridgeUrl: 'https://api.internxt.com' 
+            //     };
+
+            //     const fileObject = await new FileObjectUpload(overridenConfig, fakeFileMeta, bucketId).init();
+            //     await fileObject.StartUploadFile();
+            // } catch (err) {
+            //     console.log(err);
+            //     errorThrown = true;
+            //     expect(err.message).to.equal('Request failed with status code 404')
+            // } finally  {
+            //     // expect(errorThrown).to.be.true;
+            // }      
+        });
     });
 });
