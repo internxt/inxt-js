8d5206553036fafe2f1c7f5cd731d0124556195b
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-05-31 14:10:13 +0200

    update buikd

--- build/lib/utils/logger/logger.js
+++ build/lib/utils/logger/logger.js
@@ -49,6 +49,7 @@ var getLoggerInstance = function (level) {
         format: Winston.format.combine(Winston.format.colorize({ all: true }), Winston.format.timestamp({ format: 'YYYY-MM-DD HH:MM:SS' }), Winston.format.splat(), Winston.format.printf(function (info) { return info.timestamp + " " + info.level + ": " + info.message; })),
         transports: [new Winston.transports.Console()]
     });
+    // console.log(process.env.STAGE)
     if (process.env.STAGE !== 'development') {
         logger.silent = true;
     }
