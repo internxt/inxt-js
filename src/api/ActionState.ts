770a6442de354ae06ff90763590632eee1743d3c
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-06-01 17:10:16 +0200

    action state class to manage state

---
+++ src/api/ActionState.ts
@@ -0,0 +1,28 @@
+import EventEmitter from 'events';
+
+import { DOWNLOAD_CANCELLED, UPLOAD_CANCELLED } from './constants';
+
+export enum ActionTypes {
+    DOWNLOAD = 'DOWNLOAD',
+    UPLOAD = 'UPLOAD'
+}
+
+export class ActionState extends EventEmitter {
+    private type: ActionTypes;
+
+    constructor(type: ActionTypes) {
+        super();
+
+        this.type = type;
+    }
+
+    public stop(): void {
+        if (this.type === ActionTypes.DOWNLOAD) {
+            this.emit(DOWNLOAD_CANCELLED);
+        }
+
+        if (this.type === ActionTypes.UPLOAD) {
+            this.emit(UPLOAD_CANCELLED);
+        }
+    }
+}
