056352d9e91ad91598da5f1c54752e32594667d8
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-06-01 17:09:54 +0200

    add constants

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -1,1 +1,10 @@
 export const DEFAULT_INXT_MIRRORS = 10;
+
+/* EVENTS */
+export const DOWNLOAD_CANCELLED = 'DOWNLOAD CANCELLED';
+export const UPLOAD_CANCELLED = 'UPLOAD CANCELLED';
+
+/* ERRORS */
+export const ENCRYPTION_KEY_NOT_PROVIDED = 'Encryption key was not provided';
+export const BUCKET_ID_NOT_PROVIDED = 'Bucket id was not provided';
+export const FILE_ID_NOT_PROVIDED = 'File id was not provided';
