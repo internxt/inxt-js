8358a0090b850e00b874363d12775a667f16dbeb
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-07-09 12:07:59 +0200

    wrappedError class

--- src/lib/utils/error.ts
+++ src/lib/utils/error.ts
@@ -1,8 +1,19 @@
+class WrappedError extends Error {
+    header: string;
+
+    constructor(header: string) {
+        super();
+
+        this.header = header;
+    }
+}
+
 export const wrap = (header: string, err: Error) => {
-    const wrappedError = new Error(header + ': ' + err.message);
+    const wrappedError = new WrappedError(header + ': ' + err.message);
 
     wrappedError.stack = err.stack;
     wrappedError.name = err.name;
-    
+    wrappedError.header = header;
+
     return wrappedError;
-}
+};
