6ee4b10b97f328d6f01ba11dbbf2b17e72cdff4a
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-07-17 11:39:25 +0200

    bubble errors from upload to entrypoint

--- src/lib/upload/upload.ts
+++ src/lib/upload/upload.ts
@@ -4,6 +4,7 @@ import { EnvironmentConfig, UploadFileOptions } from "../..";
 import { ActionState } from "../../api/ActionState";
 import { UPLOAD_CANCELLED } from "../../api/constants";
 import { FileObjectUpload, FileMeta } from "../../api/FileObjectUpload";
+import { logger } from '../utils/logger';
 
 /**
  * Uploads a file to the network
@@ -13,31 +14,26 @@ import { FileObjectUpload, FileMeta } from "../../api/FileObjectUpload";
  * @param progress upload progress callback
  * @param finish finish progress callback
  */
-export async function upload(config: EnvironmentConfig, bucketId: string, fileMeta: FileMeta, params: UploadFileOptions, logger: Winston.Logger, actionState: ActionState): Promise<void> {
-    let file: FileObjectUpload;
+export async function upload(config: EnvironmentConfig, bucketId: string, fileMeta: FileMeta, params: UploadFileOptions, debug: Winston.Logger, actionState: ActionState): Promise<void> {
+    const file = new FileObjectUpload(config, fileMeta, bucketId, debug);
 
-    try {
-        file = new FileObjectUpload(config, fileMeta, bucketId, logger);
-        actionState.on(UPLOAD_CANCELLED, () => { file.emit(UPLOAD_CANCELLED); });
+    actionState.on(UPLOAD_CANCELLED, () => {
+        file.emit(UPLOAD_CANCELLED);
+    });
 
-        await file.init();
-        await file.checkBucketExistence();
-        await file.stage();
-        file.encrypt();
+    await file.init();
+    await file.checkBucketExistence();
+    await file.stage();
+    file.encrypt();
 
-        const uploadResponses = await file.upload(params.progressCallback);
+    const uploadResponses = await file.upload(params.progressCallback);
 
-        logger.debug('Upload finished. Creating bucket entry...');
+    logger.debug('Upload finished. Creating bucket entry...');
 
-        await file.createBucketEntry(uploadResponses);
+    await file.createBucketEntry(uploadResponses);
 
-        params.progressCallback(1, file.getSize(), file.getSize());
+    logger.info('Uploaded file with id %s', file.getId());
 
-        params.finishedCallback(null, file.getId());
-    } catch (err) {
-        if (file! && file!.isAborted()) {
-            return params.finishedCallback(new Error('Process killed by user'), null);
-        }
-        params.finishedCallback(err, null);
-    }
+    params.progressCallback(1, file.getSize(), file.getSize());
+    params.finishedCallback(null, file.getId());
 }
