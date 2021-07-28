be38ce6a5d54a715608af3923366494f75b1a924
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-07-28 12:59:48 +0200

    delete unused code

--- src/api/auth.ts
+++
@@ -1,8 +0,0 @@
-import { sha256 } from '../lib/crypto';
-import { EnvironmentConfig } from "../index";
-
-export function GetBasicAuth(config: EnvironmentConfig) {
-  const hash = sha256(Buffer.from(config.bridgePass)).toString('hex');
-
-  return `Basic ` + Buffer.from(`${config.bridgeUser}:${hash}`).toString('base64');
-}
