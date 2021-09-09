058cdd641f9ecf2004012b5bab65479ea5e3ca40
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-09-09 22:15:36 +0200

    upload strategy interface

---
+++ src/lib/upload/UploadStrategy.ts
@@ -0,0 +1,37 @@
+import { EventEmitter } from 'stream';
+import { ContractNegotiated } from '../contracts';
+import { ShardMeta } from '../shardMeta';
+
+export type NegotiateContract = (shardMeta: ShardMeta) => Promise<ContractNegotiated>;
+
+export interface ShardUploadSuccessMessage {
+  hash: string;
+  size: number;
+}
+
+export interface UploadFinishedMessage {
+  result: any;
+}
+
+export enum UploadEvents {
+  Error = 'upload-error',
+  Started = 'upload-start',
+  Aborted = 'upload-aborted',
+  Finished = 'upload-finished',
+  ShardUploadSuccess = 'shard-upload-success',
+}
+
+export abstract class UploadStrategy extends EventEmitter {
+  fileEncryptionKey = Buffer.alloc(0);
+  iv = Buffer.alloc(0);
+  abstract getIv(): Buffer;
+  abstract setIv(iv: Buffer): void;
+  abstract getFileEncryptionKey(): Buffer;
+  abstract setFileEncryptionKey(fk: Buffer): void;
+  abstract upload(negotiateContract: NegotiateContract): void;
+  abstract abort(): void;
+}
+
+export interface UploadParams {
+  desiredRamUsage: number;
+}
