c3ba7345dec82a2ab6404dcbf83e0744ea1e3541
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-09-16 17:27:01 +0200

    add doc to upload strategy

--- src/lib/upload/UploadStrategy.ts
+++ src/lib/upload/UploadStrategy.ts
@@ -26,11 +26,38 @@ export enum UploadEvents {
 export abstract class UploadStrategy extends EventEmitter implements Abortable {
   fileEncryptionKey = Buffer.alloc(0);
   iv = Buffer.alloc(0);
+
+  /**
+   * Should return the initialization vector used for file encryption
+   */
   abstract getIv(): Buffer;
+
+  /**
+   * Should set the required iv to perform an encryption
+   * @param iv Initialization vector used in file encryption
+   */
   abstract setIv(iv: Buffer): void;
+
+  /**
+   * Should return the file encryption key
+   */
   abstract getFileEncryptionKey(): Buffer;
+
+  /**
+   * Should set the file encryption key
+   * @param fk File encryption key used to encrypt a file
+   */
   abstract setFileEncryptionKey(fk: Buffer): void;
+
+  /**
+   * Should execute the steps to perform an upload
+   * @param negotiateContract Injected method to negotiate a contract
+   */
   abstract upload(negotiateContract: NegotiateContract): void;
+
+  /**
+   * Should abort the upload strategy as soon as possible
+   */
   abstract abort(): void;
 }
 
