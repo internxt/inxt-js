bec90033cae4db3fe315390cf14658a595842de6
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-09-18 11:19:27 +0200

    download-file cli command

---
+++ src/cli/download-file.ts
@@ -0,0 +1,50 @@
+import { createWriteStream } from "fs";
+import { pipeline, Readable } from "stream";
+import { logger } from "../lib/utils/logger";
+import { getEnvironment } from "./CommandInterface";
+
+export async function downloadFile(fileId: string, path: string) {
+  logger.info('Donwloading file %s', fileId);
+
+  const network = getEnvironment();
+  const bucketId = process.env.BUCKET_ID;
+
+  await new Promise((resolve, reject) => {
+    const state = network.download(bucketId, fileId, {
+      progressCallback: (progress: number) => {
+        logger.info('Progress: %s %', (progress * 100).toFixed(2));
+      },
+      finishedCallback: (err: Error | null, downloadStream: Readable | null) => {
+        if (err) {
+          return reject(err);
+        }
+
+        pipeline(downloadStream as Readable, createWriteStream(path), (err) => {
+          if (err) {
+            return reject(err);
+          }
+
+          resolve(null);
+        });
+      },
+      debug: (msg: string) => {
+        logger.debug('DEBUG', msg);
+      }
+    }, {
+      label: 'OneStreamOnly',
+      params: {}
+    });
+
+    process.on('SIGINT', () => {
+      network.resolveFileCancel(state);
+    });
+  }).then(() => {
+    logger.info('File downloaded on path %s', path);
+
+    process.exit(0);
+  }).catch((err) => {
+    logger.error('Error uploading file %s', err.message);
+
+    process.exit(1);
+  });
+}
