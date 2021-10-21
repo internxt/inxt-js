8ec83c674c759abd5905d0588ae48e1b1eb794ad
Sergio Gutiérrez Villalba <gv.sergio@gmail.com>
2021-10-21 12:29:16 +0200

    catch errors on upload-file command

--- src/cli/upload-file.ts
+++ src/cli/upload-file.ts
@@ -5,41 +5,46 @@ import { UploadOptions } from '../lib/upload';
 import { logger } from '../lib/utils/logger';
 import { getEnvironment } from './CommandInterface';
 
-export function uploadFile(filepath: string) {
+export async function uploadFile(filepath: string) {
   if (!existsSync(filepath)) {
     logger.error('File "%s" does not exist', filepath);
-    process.exit(0);
+    process.exit(-1);
   }
 
-  const network = getEnvironment();
-  const bucketId = process.env.BUCKET_ID;
+  try {
+    const network = getEnvironment();
+    const bucketId = process.env.BUCKET_ID;
 
-  const uploadStrategy: UploadStrategyObject = {
-    label: 'OneStreamOnly',
-    params: { 
-      desiredRamUsage: 200 * 1024 * 1024, 
-      source: {
-        stream: createReadStream(filepath),
-        hash: '',
-        size: statSync(filepath).size
-      } 
-    }
-  };
-
-  return new Promise((resolve, reject) => {
-    const uploadOpts: UploadOptions = {
-      filename: filepath,
-      progressCallback: (progress: number) => {
-        logger.debug('Progress %s%', (progress * 100).toFixed(2));
-      },
-      finishedCallback: (err: Error | null, res: string | null) => {
-        if (err) {
-          return reject(err);
-        }
-        resolve(res);
+    const uploadStrategy: UploadStrategyObject = {
+      label: 'OneStreamOnly',
+      params: { 
+        desiredRamUsage: 200 * 1024 * 1024, 
+        source: {
+          stream: createReadStream(filepath),
+          hash: '',
+          size: statSync(filepath).size
+        } 
       }
     };
 
-    network.upload(bucketId, uploadOpts, uploadStrategy);
-  });
+    await new Promise((resolve, reject) => {
+      const uploadOpts: UploadOptions = {
+        filename: filepath,
+        progressCallback: (progress: number) => {
+          logger.debug('Progress %s%', (progress * 100).toFixed(2));
+        },
+        finishedCallback: (err: Error | null, res: string | null) => {
+          if (err) {
+            return reject(err);
+          }
+          resolve(res);
+        }
+      };
+
+      network.upload(bucketId, uploadOpts, uploadStrategy);
+    });
+  } catch (err) {
+    logger.error('Error uploading file: %s', err.message);
+    process.exit(-1);
+  }  
 }
