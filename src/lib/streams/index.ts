91537c85b76d9412be615d035c5544eff47e438c
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-10-23 21:30:48 +0200

    move files to utils

--- src/lib/streams/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './BytesCounter';
-export * from './ProgressNotifier';
--- src/lib/utils/stream.ts
+++
@@ -1,5 +0,0 @@
-import { Transform } from 'stream';
-
-export function drainStream(stream: Transform) {
-    return new Promise(r => stream.once('drain', r));
-}
