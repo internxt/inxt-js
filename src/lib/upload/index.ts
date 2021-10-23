3c371c59ec32fa68f7d7f00d58f18ccd884756d9
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-10-23 17:06:09 +0200

    remove old upload/, download/

--- src/lib/upload/index.ts
+++
@@ -1,8 +0,0 @@
-import { UploadFileOptions } from '../..';
-
-export interface UploadOptions extends UploadFileOptions {
-  filename: string;
-}
-
-export * from './upload';
-export * from './OneStreamStrategy';
