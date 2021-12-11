c0a96caef733d409fb42644bc5bc30ebdd561dd8
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-12-11 13:24:54 +0100

    bucket interface

---
+++ src/api/Bucket.ts
@@ -0,0 +1,12 @@
+export interface Bucket {
+  id: string
+  user: string
+  encryptionKey: string
+  publicPermissions: string[]
+  created: Date
+  name: string
+  pubkeys: string[]
+  status: string
+  transfer: number,
+  storage: number
+}
