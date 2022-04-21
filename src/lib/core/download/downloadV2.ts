ae7ebe88d798b952cb2c2f9f3001713847ddbadd
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-04-21 16:27:14 +0200

    feat(download): v2 network support

---
+++ src/lib/core/download/downloadV2.ts
@@ -0,0 +1,108 @@
+import { createDecipheriv, randomBytes } from 'crypto';
+import { PassThrough, Readable } from 'stream';
+import { pipeline } from 'stream/promises';
+
+import { ALGORITHMS, DecryptFileFunction, DownloadFileFunction, Network } from '@internxt/sdk/dist/network';
+import { downloadFile } from '@internxt/sdk/dist/network/download';
+
+import { getStream } from '../../../services/request';
+import { GenerateFileKey, sha256 } from '../../utils/crypto';
+import { Events as ProgressEvents, HashStream, ProgressNotifier } from '../../utils/streams';
+import { DownloadProgressCallback } from '.';
+import { ActionState } from '../../../api';
+import { Events } from '..';
+import Errors from './errors';
+
+export async function downloadFileV2(
+  fileId: string,
+  bucketId: string,
+  mnemonic: string,
+  bridgeUrl: string,
+  creds: { pass: string, user: string },
+  notifyProgress: DownloadProgressCallback,
+  actionState: ActionState
+): Promise<PassThrough> {
+  const abortController = new AbortController();
+
+  actionState.once(Events.Download.Abort, () => {
+    abortController.abort();
+  });
+
+  const outStream = new PassThrough();
+  const network = Network.client(bridgeUrl, {
+    clientName: 'inxt-js',
+    clientVersion: '1.0'
+  }, {
+    bridgeUser: creds.user,
+    userId: sha256(Buffer.from(creds.pass)).toString('hex')
+  });
+
+  const fileEncryptedSlices: {
+    stream: Readable,
+    hash: string
+  }[] = [];
+
+  const downloadFileStep: DownloadFileFunction = async (downloadables) => {
+    for (const downloadable of downloadables.sort((dA, dB) => dA.index - dB.index)) {
+      fileEncryptedSlices.push({
+        hash: downloadable.hash,
+        stream: await getStream(downloadable.url)
+      });
+    }
+  };
+
+  const decryptFileStep: DecryptFileFunction = async (algorithm, key, iv, fileSize) => {
+    if (algorithm !== ALGORITHMS.AES256CTR.type) {
+      throw Errors.downloadUnknownAlgorithmError;
+    }
+
+    const decipher = createDecipheriv('aes-256-ctr', key as Buffer, iv as Buffer);
+    const progress = new ProgressNotifier(fileSize, 2000, { emitClose: false });
+
+    progress.on(ProgressEvents.Progress, (progress: number) => {
+      notifyProgress(progress, null, null);
+    });
+
+    for (const fileEncryptedSlice of fileEncryptedSlices) {
+      const hasher = new HashStream();
+
+      await pipeline(fileEncryptedSlice.stream, hasher, decipher, progress, outStream, {
+        signal: abortController.signal
+      });
+
+      const calculatedHash = hasher.getHash().toString('hex');
+      const expectedHash = fileEncryptedSlice.hash;
+
+      if (calculatedHash !== expectedHash) {
+        throw Errors.downloadHashMismatchError; 
+      }
+    }
+
+    await new Promise((res) => progress.end(res));
+  };
+
+  const downloadPromise = (async () => {
+    await downloadFile(
+      fileId, 
+      bucketId, 
+      mnemonic, 
+      network, 
+      {
+        algorithm: ALGORITHMS.AES256CTR,
+        randomBytes,
+        generateFileKey: (mnemonic, bucketId, index) => {
+          return GenerateFileKey(mnemonic, bucketId, index as Buffer | string);
+        }
+      }, 
+      Buffer.from, 
+      downloadFileStep, 
+      decryptFileStep
+    );
+  })();
+
+  downloadPromise.catch((err) => {
+    outStream.emit('error', err);
+  });
+
+  return outStream;
+}
