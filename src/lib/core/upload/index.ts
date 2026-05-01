689c495b6408e3807682f47898c0104e1afc3521
Daniel Jiménez Rivera <dani@internxt.com>
2026-05-01 12:33:26 +0200

    Update index.ts

--- src/lib/core/upload/index.ts
+++ src/lib/core/upload/index.ts
@@ -7,9 +7,11 @@ export type UploadProgressCallback = (
   uploadedBytes: number | null,
   totalBytes: number | null,
 ) => void;
+export type EncryptProgressCallback = (progress: number) => void;
 
 export interface UploadOptions {
   progressCallback: UploadProgressCallback;
+  encryptProgressCallback?: EncryptProgressCallback;
   fileSize: number;
   source: Readable;
   abortSignal?: AbortSignal;
