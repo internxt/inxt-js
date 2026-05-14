4a13e3ee517d8d25a88372349e74a7fdcccacc2e
Daniel Jiménez Rivera <dani@internxt.com>
2026-05-14 15:16:10 +0100

    Fix tsconfig target (#134)

--- src/services/request.ts
+++ src/services/request.ts
@@ -1,8 +1,6 @@
-import * as url from 'url';
 import * as https from 'https';
 import * as http from 'http';
 import { Readable } from 'stream';
-import { ClientRequest, IncomingMessage } from 'http';
 import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
 
 import { EnvironmentConfig } from '../api';
@@ -42,61 +40,6 @@ export async function request(
     }
 
     return value;
-  });
-}
-
-export function streamRequest(targetUrl: string, timeoutSeconds?: number): Readable {
-  const uriParts = url.parse(targetUrl);
-  let downloader: ClientRequest | null = null;
-
-  function _createDownloadStream(): ClientRequest {
-    const requestOpts = {
-      protocol: uriParts.protocol,
-      hostname: uriParts.hostname,
-      port: uriParts.port,
-      path: uriParts.path,
-      headers: {
-        Accept: 'application/octet-stream',
-      },
-    };
-
-    return uriParts.protocol === 'http:' ? http.get(requestOpts) : https.get(requestOpts);
-  }
-
-  return new Readable({
-    read() {
-      if (!downloader) {
-        downloader = _createDownloadStream();
-
-        if (timeoutSeconds) {
-          downloader.setTimeout(timeoutSeconds * 1000, () => {
-            downloader?.destroy(Error(`Request timeouted after ${timeoutSeconds} seconds`));
-          });
-        }
-
-        this.once('signal', (message: string) => {
-          if (message === 'Destroy request') {
-            downloader?.destroy();
-          }
-
-          this.destroy();
-        });
-
-        downloader
-          .on('response', (res: IncomingMessage) => {
-            res
-              .on('data', this.push.bind(this))
-              .on('error', this.emit.bind(this, 'error'))
-              .on('end', () => {
-                this.push.bind(this, null);
-                this.emit('end');
-              })
-              .on('close', this.emit.bind(this, 'close'));
-          })
-          .on('error', this.emit.bind(this, 'error'))
-          .on('timeout', () => this.emit('error', Error('Request timeout')));
-      }
-    },
   });
 }
 
